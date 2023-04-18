82618361633b1aa58094a70ee1bb6c1a0cdb1387
garry彭 <250631521@qq.com>
2023-04-18 14:02:57 +0800

    完善service active

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# 使用 Service Workder 实现离线化
+
+# 参考资料
+
+> https://developer.mozilla.org/zh-CN/docs/Web/API/Service_Worker_API
+> https://github.com/mdn/serviceworker-cookbook/
+> https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
--- sw.js
+++ sw.js
@@ -1,70 +1,83 @@
+/*
+ * Copyright © 2020-2023 Ocean Galaxy Inc. All Rights Reserved.
+ * @Description:
+ * @LastEditors: garry彭
+ * @LastEditTime: 2023-04-18 14:01:32
+ */
+const pageName = 'myPageName'
+const cacheKey = `${pageName}-${Date.now()}}`
+
 // 把响应资源写入缓存
 // 只缓存 .js、.css 和图片文件
 const putInCache = async (request, response) => {
-  // 每次页面资源有更新，只用更新一下open的参数即可
-  const cache = await caches.open("yourPageName-version");
+  const cache = await caches.open(cacheKey)
 
   if (
-    /\/yourPageName/.test(request.url) ||
+    new RegExp(`/${pageName}`).test(request.url) ||
     /\.(js|css|png|ico)/.test(request.url)
   ) {
-    await cache.put(request, response);
+    await cache.put(request, response)
   }
-};
+}
 
 // 清除旧缓存
 const deleteOldCaches = async () => {
-  const keyList = await caches.keys();
+  const keyList = await caches.keys()
+
+  const cache = await caches.open(cacheKey)
+  const reg = new RegExp(`^${pageName}-\\d{13}`)
 
   await Promise.all(
     keyList.map(async (key) => {
-      await caches.delete(key);
+      if (reg.test(key) && !key.includes(cacheKey)) {
+        await cache.delete(key)
+      }
     })
-  );
-};
+  )
+}
 
 const cacheFirst = async ({ request, fallbackUrl }) => {
   // 从缓存中换取资源
-  const responseFromCache = await caches.match(request);
+  const responseFromCache = await caches.match(request)
   if (responseFromCache) {
-    return responseFromCache;
+    return responseFromCache
   }
 
   // 如果 preload 也没有命中，则从网络中获取
   try {
-    const responseFromNetwork = await fetch(request);
+    const responseFromNetwork = await fetch(request)
 
-    putInCache(request, responseFromNetwork.clone());
+    putInCache(request, responseFromNetwork.clone())
 
-    return responseFromNetwork;
+    return responseFromNetwork
   } catch (error) {
     if (fallbackUrl) {
-      const fallbackResponse = await caches.match(fallbackUrl);
+      const fallbackResponse = await caches.match(fallbackUrl)
 
       if (fallbackResponse) {
-        return fallbackResponse;
+        return fallbackResponse
       }
     }
 
-    return new Response("Network error happened", {
+    return new Response('Network error happened', {
       status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
+      headers: { 'Content-Type': 'text/plain' }
+    })
   }
-};
+}
 
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(deleteOldCaches());
-});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(deleteOldCaches())
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     cacheFirst({
-      request: event.request,
+      request: event.request
     })
-  );
-});
+  )
+})
